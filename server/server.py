d4381de11b30843c66c52e77473467cc716073a9
Lee <294622946@qq.com>
2022-02-22 15:15:04 +0800

    fixed: init .sc-conf.yaml

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 import logging
+import os
 
 import click
 from tornado.ioloop import IOLoop
 from tornado.log import enable_pretty_logging
 
 import db
+import settings
 from app import make_app
 
 enable_pretty_logging()
@@ -12,11 +14,18 @@ enable_pretty_logging()
 logger = logging.getLogger(__name__)
 
 
+def initialize_config_file():
+    """初始化配置文件"""
+    open(os.path.join(settings.SCRIPTS_DIR, settings.CONFIG_FILENAME), 'a').close()
+
+
 @click.command()
 @click.option('--host', default='127.0.0.1')
 @click.option('--port', default=8310, type=click.INT)
 @click.option('--debug', is_flag=True)
 def main(host, port, debug):
+    initialize_config_file()
+
     app = make_app(debug)
     app.listen(port, host)
 
